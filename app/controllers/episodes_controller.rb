1ab576db6094a724e2c3da35e8da96b0f970bd18
Danial Pearce <git@tigris.id.au>
2009-02-07 19:57:19 +1100

    Ninjad a basic design from some free template site

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,3 +1,17 @@
 class EpisodesController < ApplicationController
   before_filter :login_required
+
+  def index
+    @movies = current_user.movies
+  end
+
+  def new
+    @movie = Episode.new
+  end
+
+  def create
+    @episode = Episode.create params[:episode]
+    @media = @episode.media.create params[:episode].merge(:user => current_user)
+    redirect_to '/'
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,5 +12,6 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create params[:movie]
     @media = @movie.media.create params[:media].merge(:user => current_user)
+    redirect_to '/'
   end
 end
