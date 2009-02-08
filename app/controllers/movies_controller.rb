6d01ee173772c7eeac80a7e736bc90e3bee83035
Danial Pearce <git@tigris.id.au>
2009-02-08 16:45:26 +1100

    Search support

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,17 @@ class MoviesController < ApplicationController
   before_filter :login_required
 
   def index
-    @movies = current_user.movies
+    # TODO: I hate building search constructs like this. Brings back memories of
+    # DBI perl days. Does A::R support like searches somehow? I'll probably be
+    # converting to sphinx/ferret at some point anyway, even if it is overkill
+    # to run a whole external daemon for ~1000 records.
+    search_terms = {}
+    sql = '1=1'
+    unless params['title'].nil?
+      sql += %q( and lower(title) like :title)
+      search_terms[:title] = "%#{params[:title].downcase}%"
+    end
+    @movies = current_user.movies.find(:all, :conditions => [ sql, search_terms ]) unless search_terms.blank?
   end
 
   def new
